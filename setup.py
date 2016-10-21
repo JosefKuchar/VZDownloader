586e8457e41bf6959f0471f477dcf502323c3e37
Josef Kuchař <email@josefkuchar.cz>
2016-10-21 19:14:20 +0200

    Setup and comments

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "vzdownload",
+    version = "1.0.0",
+    author = "Josef Kuchař",
+    author_email = "email@josefkuchar.cz",
+    description = "Tool for creating song-book from velkyzpevnik.cz",
+    license = "GPLv3",
+    keywords = "downloader tool python",
+    url = "https://github.com/JosefKuchar/VZDownloader",
+    long_description = read("readme.md"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: GNUv3 License"
+    ],
+    install_requires = [
+        "colorama",
+        "configparser",
+        "pdflib"
+    ]
+)
--- vzdownloader.py
+++ vzdownloader.py
@@ -1,34 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Import libraries
+# Internal
 from urllib.request import urlopen
 import threading
+# External
 import configparser
 import colorama
-#pdflib, configparser, colorama
+import pdflib
 
+# Define functions
 def patchFile(string):
-    #Fix head
+    # Fix head
     string = string.replace('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">', '<!doctype html>')
     string = string.replace('<html xmlns="http://www.w3.org/1999/xhtml" lang="cs">', '<html>')
     string = string.replace('<meta http-equiv="content-type" content="text/html;charset=UTF-8"/>', '<meta charset="UTF-8">')
 
-    #Disable print dialog
+    # Disable print dialog
     string = string.replace('<body onload="window.print()">', '<body>')
 
-    #Remove AD in bottom
+    # Remove AD in bottom
     string = string.replace("<p class='footer'>Další texty písniček s akordy najdete na serveru <b>http://www.velkyzpevnik.cz</b></p>", '')
     string = string.replace(".footer { padding-top:1em; border-top:1px solid black; font-size:9pt; text-align:center; }", '')
 
     return string
 
-#Init colors
+# Init colors
 colorama.init();
 
-#Print logo
+# Print logo
 print(colorama.Fore.YELLOW + " _____ _____ " + colorama.Fore.WHITE + "   ____                _           _         ")
 print(colorama.Fore.YELLOW + "|  |  |__   |" + colorama.Fore.WHITE + "  |    \ ___ _ _ _ ___| |___ ___ _| |___ ___ ")
 print(colorama.Fore.YELLOW + "|  |  |   __|" + colorama.Fore.WHITE + "  |  |  | . | | | |   | | . | .'| . | -_|  _|")
 print(colorama.Fore.YELLOW + " \___/|_____|" + colorama.Fore.WHITE + "  |____/|___|_____|_|_|_|___|__,|___|___|_|  ")
 
-#Get number of songs from site
+# Get number of songs from site
 line = urlopen("http://www.velkyzpevnik.cz/zpevnik").readlines()[22].decode('utf-8')
 songCount = int(line[line.find("<b>")+4:][:line[line.find("<b>")+4:].index(" ")])
 songsPercent = songCount;
